d8cc41ad52e93b7b170df1535f1f89e53de5a63a
Bhavani-93807 <bhavani93807@gmail.com>
2024-08-09 22:45:26 +0530

    update: buttons code has been updated

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,39 +1,63 @@
-import React,{useState} from 'react'
-import '../App.css';
+import React, { useState } from 'react';
+import { evaluate } from 'mathjs'; // Install this package using `npm install mathjs`
+
 
 const Calculator = () => {
-const [input, setInput] = useState("");
-const [result, setResult] = useState(0);
-const handler = e => {
-    setInput(e.target.value);
-}
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    if (value === 'Ans') {
+      setInput(input + result);
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  const calculateResult = () => {
+    try {
+      const evaluatedResult = evaluate(input);
+      setResult(evaluatedResult);
+      setInput(evaluatedResult.toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const buttons = [
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['0', 'Ans','clr'],
+    ['clearstore', 'view history']
+    
+  ];
+
   return (
-  
-    <div>
-       <center>
-       <input type="text" value={input} name="input" onChange={handler}/>
-       <br />
-       <button onClick={() => setResult(eval(input))}>Result</button>
-       <h4>Result is here : {result}</h4>
-        <button onClick={() => setInput(input+'1')}>1</button>
-        <button onClick={() => setInput(input+'2')}>2</button>
-        <button onClick={() => setInput(input+'3')}>3</button>
-        <button onClick={() => setInput(input+'4')}>4</button>
-        <button onClick={() => setInput(input+'5')}>5</button> <br />
-        <button onClick={() => setInput(input+'6')}>6</button>
-         <button onClick={() => setInput(input+'7')}>7</button>
-        <button onClick={() => setInput(input+'8')}>8</button>
-        <button onClick={() => setInput(input+'9')}>9</button>
-        <button onClick={() => setInput(input+'0')}>0</button><br />
-
-        <button onClick={() => setInput(input+'+')}>+</button>
-        <button onClick={() => setInput(input+'-')}>-</button>
-        <button onClick={() => setInput(input+'*')}>*</button>
-        <button onClick={() => setInput(input+'/')}>/</button>
-        <button onClick={() => setInput('')}>clr</button>
-       </center>
+    <div className="calculator-container">
+      <center>
+        <input type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br />
+        <button onClick={calculateResult}>Result</button>
+        <h4>Result is here: {result}</h4>
+        {buttons.map((row, rowIndex) => (
+          <div className="button-row" key={rowIndex}>
+            {row.map((btn, index) => (
+              <button
+                key={index}
+                onClick={() => btn === 'clr' ? setInput('') : handleClick(btn)}
+              >
+                {btn}
+              </button>
+            ))}
+          </div>
+        ))}
+      </center>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
