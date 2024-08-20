6e920c514dab85e6489f167d1ae90aac3e415855
Bhavani-93807 <bhavani93807@gmail.com>
2024-08-20 16:39:36 +0530

    feat: React Router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
+import React from "react";
 
-import React from 'react'
-import Axiosfunction from './components/Axiosfunction'
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <Axiosfunction />
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
