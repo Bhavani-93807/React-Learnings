e29e54e39a01526cd5f4cf0a3eff4a307bf5944c
Bhavani-93807 <bhavani93807@gmail.com>
2024-08-20 16:38:53 +0530

    feat: React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
-import React from 'react'
-import Axiosfunction from './components/Axiosfunction'
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div>
-      <Axiosfunction />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Header} />
+          <Route path="about/" exact component={About} />
+          <Route path="dashboard/" exact component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      Header component
+       <h4>Welcome to HomePage!!</h4>
     </div>
   )
 }
